09680fe5f9b53412a2a50d02b368f6e8c17459a5
Daniel Chen <Maveri2k@yahoo.com>
2014-10-02 15:27:14 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='pypi_test',
+      packages=['pypi_test'],  # this must be the same as the name above
+      version='0.0.1',
+      description='A random test lib',
+      author='Daniel Chen',
+      author_email='example@example.com',
+      url='https://github.com/chendaniely/pypi_test',
+      download_url='https://github.com/chendaniely/pypi_test/tarball/0.0.1',
+      keywords=['testing', 'logging', 'example'],
+      classifiers=[])
